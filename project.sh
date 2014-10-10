c6bdcbb1ad1e0e869eb5783b16846a72d6fbf349
Ben Casey <bcasey@mines.edu>
2014-10-10 16:13:05 -0600

    initial set up with 1 table

---
+++ project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+	sqlite3 project.db ".dump"
+	rm project.db
+	sqlite3 project.db < customersScript
+	sqlite3 project.db "insert into customers (id,name,address,age,accnum) values (1,'Anderson','12 Arapahoe st',23,4576)"
+
