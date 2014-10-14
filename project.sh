bffd6a34caed087bf3ef5df2a99da373756f1ef8
Ben Casey <bcasey@mines.edu>
2014-10-14 14:44:21 -0600

    added items and orders tables

--- project.sh
+++ project.sh
@@ -2,5 +2,7 @@
 	sqlite3 project.db ".dump"
 	rm project.db
 	sqlite3 project.db < customersScript
+	sqlite3 project.db < itemsScript
+	sqlite3 project.db < ordersScript
 	sqlite3 project.db "insert into customers (id,name,address,age,accnum) values (1,'Anderson','12 Arapahoe st',23,4576)"
 
